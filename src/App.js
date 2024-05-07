bdffad37443f1ea026bea497fb53545348df0e24
Ali Mousazada <mousazada.ali@gmail.com>
2024-05-06 21:33:20 -0400

    using react hook form to validate and catching data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import SubmitForm from './submitForm';
 
 function App() {
   return (
     <div className="App">
-      
-      
+
+      <SubmitForm />
+
     </div>
   );
 }
